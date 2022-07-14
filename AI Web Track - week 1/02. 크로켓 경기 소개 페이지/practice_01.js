5468ec15116112aaa6e3c6e3f22eab1a1d598692
Hyyena <sjtxm0320@gmail.com>
2022-07-15 00:28:03 +0900

    home_work

--- AI Web Track - week 1/02. 크로켓 경기 소개 페이지/practice_01.js
+++ AI Web Track - week 1/02. 크로켓 경기 소개 페이지/practice_01.js
@@ -1,19 +1,16 @@
 function getCheck() {
-
     const foodList = document.getElementsByName("food");
     let str = "";
 
     for (let i = 0; i < foodList.length; i++) {
-
         const target = foodList[i];
 
         if (target.checked == true) {
-            str += target.value + "<br/>"; 
+            str += target.value + "<br/>";
         }
     }
 
     document.getElementById("result").innerHTML = "<h1>" + str + "</h1>";
-
 }
 
 const btn = document.getElementById("btn");
--- AI Web Track - week 1/02. 크로켓 경기 소개 페이지/review_01.js
+++ AI Web Track - week 1/02. 크로켓 경기 소개 페이지/review_01.js
@@ -2,18 +2,15 @@ var btnPlus = document.getElementById("plus");
 var btnMinus = document.getElementById("minus");
 
 function plus() {
-
     var input = document.getElementById("input").value;
     var output = Number(input) + 1;
 
     console.log(output);
 
     document.getElementById("input").value = output;
-
 }
 
 function minus() {
-
     var input = document.getElementById("input").value;
     var output = Number(input) - 1;
     if (output < 1) {
@@ -23,7 +20,6 @@ function minus() {
     console.log(output);
 
     document.getElementById("input").value = output;
-
 }
 
 btnPlus.addEventListener("click", plus);
