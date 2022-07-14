26d622744fd730317da699c2ee03b84f17154726
Hyyena <sjtxm0320@gmail.com>
2022-07-15 00:27:50 +0900

    home_work

--- AI Web Track - week 1/test.js
+++ AI Web Track - week 1/test.js
@@ -1,13 +1,65 @@
-// 1. 아이디가 "carrot"인 요소를 선택하세요
-var carrot_btn = document.getElementById("carrot");
-
-// 2. click 할 시 텍스트를 보여주는 함수를 작성합니다.
-// 버튼을 클릭하면 "토끼가 나타났어요!!" 문장을 출력하도록 해보세요
-
-function showText() {
-    document.getElementById("text").innerHTML = "토끼가 나타났어요!!";
-
+// 4) Click Image Slider
+document.querySelector(".right-arrow").onclick = function () {
+    var currentSlide = document.querySelector("#photo .slide.active");
+    var nextSlide = currentSlide.nextElementSibling;
+    if (nextSlide === null) {
+        nextSlide = currentSlide.parentElement.firstElementChild;
+    }
+    currentSlide.animate(
+        {
+            opacity: [1, 0],
+        },
+        {
+            duration: 500,
+            easing: "ease",
+            iterations: 1,
+            fill: "both",
+        }
+    );
+    currentSlide.classList.remove("active");
+    nextSlide.animate(
+        {
+            opacity: [0, 1],
+        },
+        {
+            duration: 500,
+            easing: "ease",
+            iterations: 1,
+            fill: "both",
+        }
+    );
+    nextSlide.classList.add("active");
 };
 
-// 3. 선택한 요소에 "click" 이벤트 핸들러를 등록하세요 (이벤트명:"click", 함수이름)
-carrot_btn.addEventListener("click", showText);  
+//왼쪽 이미지 슬라이드 기능 구현
+document.querySelector(".left-arrow").onclick = function () {
+    var currentSlide = document.querySelector("#photo .slide.active");
+    var nextSlide = currentSlide.previousElementSibling;
+    if (nextSlide === null) {
+        nextSlide = currentSlide.parentElement.lastElementChild;
+    }
+    currentSlide.animate(
+        {
+            opacity: [1, 0],
+        },
+        {
+            duration: 500,
+            easing: "ease",
+            iterations: 1,
+            fill: "both",
+        }
+    );
+    currentSlide.classList.remove("active");
+    nextSlide.animate(
+        {
+            opacity: [0, 1],
+        },
+        {
+            duration: 500,
+            easing: "ease",
+            iterations: 1,
+            fill: "both",
+        }
+    );
+    nextSlide.classList.add("active");
+};
